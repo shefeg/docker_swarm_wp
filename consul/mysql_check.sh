5e8bb4db0e296fae1e5056803e2449b79d2c7847
root <a.ignatenko1988@gmail.com>
2018-04-11 08:25:19 +0000

    Fix mysql check

--- consul/mysql_check.sh
+++ consul/mysql_check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-MYSQL_USER = "$(cat $MYSQL_USER_FILE)"
-MYSQL_PASSWORD = "$(cat $MYSQL_PASSWORD_FILE)"
-/usr/bin/mysqladmin ping -h db -u ${MYSQL_USER} -p${MYSQL_PASSWORD}
+MYSQL_USER="$(cat $MYSQL_USER_FILE)"
+MYSQL_PASSWORD="$(cat $MYSQL_PASSWORD_FILE)"
+/usr/bin/mysqladmin ping -h db -u${MYSQL_USER} -p${MYSQL_PASSWORD} 2>&1 | grep -i "alive" || exit 1
